7789b16eccf0194f5812d6a5db9cf979c5ad46a5
farrelzayyandzaky-bit <farrelzayyandzaky@gmail.com>
2026-03-17 04:52:38 +0700

    Update Mahasiswa.java

--- Pekan 04/Mahasiswa.java
+++ Pekan 04/Mahasiswa.java
@@ -9,17 +9,14 @@
  */
 public class Mahasiswa {
 
-    // Modifier private
     private String nama;
     private int umur;
 
-    // Constructor
     public Mahasiswa(String nama, int umur) {
-        this.nama = nama;   // this keyword
+        this.nama = nama;   
         this.umur = umur;
     }
 
-    // Method public
     public void tampilkanData() {
         System.out.println("Nama : " + nama);
         System.out.println("Umur : " + umur);
@@ -27,10 +24,8 @@ public class Mahasiswa {
 
     public static void main(String[] args) {
 
-        // Membuat objek
         Mahasiswa mhs1 = new Mahasiswa("Farrel Zayyan Dzaki Muhammaad", 20);
 
-        // Menampilkan data
         mhs1.tampilkanData();
     }
 }
